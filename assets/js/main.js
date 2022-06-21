fdbe7567c6339350c0494a35cfc20901fc3072bb
raphael <paulberthet94@gmail.com>
2022-06-21 09:10:27 +0200

    modification media-queries.css et creation form.html + formulaire.css

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// Vérifier que le contenu de la page est bien chargée.
+// IMPORTANT : Mettre toute notre logique dans cet écouteur
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("chargé");
+  
+
+  });
